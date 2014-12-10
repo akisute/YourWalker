e378766fc0b693001a65f41c9ac9f23c31550b30
akisute <akisutesama@gmail.com>
2014-12-10 13:16:29 +0900

    Integrated Crashlytics (Fabric) into project
    Seems like working, but not sure

--- YourWalker/AppDelegate.swift
+++ YourWalker/AppDelegate.swift
@@ -7,11 +7,17 @@
 //
 
 import UIKit
+import Fabric
+import Crashlytics
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
+        Fabric.with([Crashlytics()])
+        return true
+    }
 }
 
