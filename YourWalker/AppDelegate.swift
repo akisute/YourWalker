96679cd0b59a0390101b7ba2d68e1c7c06999a35
Ono Masashi <masashi.ono@linecorp.com>
2014-12-15 20:45:24 +0900

    Turned off Fabric (Crashlystic) for now

--- YourWalker/AppDelegate.swift
+++ YourWalker/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
-        Fabric.with([Crashlytics()])
+        //Fabric.with([Crashlytics()])
         
         let settings = UIUserNotificationSettings(forTypes: .Badge, categories: nil)
         UIApplication.sharedApplication().registerUserNotificationSettings(settings)
