148e31b956f84713ebd0fd5b1a9bed63f301ebea
Ono Masashi <masashi.ono@linecorp.com>
2014-12-18 16:57:16 +0900

    SwiftASL: Implementing ASLViewControllerCell

--- Vendor/SwiftASL/ASLViewController.swift
+++ Vendor/SwiftASL/ASLViewController.swift
@@ -8,20 +8,73 @@
 
 import UIKit
 
-private class __ASLTableViewCell: UITableViewCell {
+class __ASLTableViewCell: UITableViewCell {
     
-    private required init(coder aDecoder: NSCoder) {
+    var timestampLabel: UILabel!
+    var messageLabel: UILabel!
+    
+    var line: ASLLine? {
+        didSet {
+            if let l = self.line {
+                self.timestampLabel.attributedText = __ASLTableViewCell.attributedStringForTimestampLabel(l)
+                self.messageLabel.text = l.message
+            } else {
+                self.timestampLabel.attributedText = nil
+                self.messageLabel.text = nil
+            }
+        }
+    }
+    
+    required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         self.commonInitialize()
     }
     
-    private override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         self.commonInitialize()
     }
     
-    private func commonInitialize() {
+    func commonInitialize() {
         self.selectionStyle = .None
+        
+        self.timestampLabel = UILabel(frame: CGRectZero)
+        self.timestampLabel.setTranslatesAutoresizingMaskIntoConstraints(false)
+        self.timestampLabel.font = nil
+        self.timestampLabel.numberOfLines = 1
+        self.timestampLabel.textAlignment = .Right
+        self.timestampLabel.backgroundColor = UIColor.redColor()
+        self.contentView.addSubview(self.timestampLabel)
+        
+        self.messageLabel = UILabel(frame: CGRectZero)
+        self.messageLabel.setTranslatesAutoresizingMaskIntoConstraints(false)
+        self.messageLabel.font = nil
+        self.messageLabel.numberOfLines = 0
+        self.messageLabel.textAlignment = .Left
+        self.messageLabel.backgroundColor = UIColor.orangeColor()
+        self.contentView.addSubview(self.messageLabel)
+        
+        let views = ["timestampLabel": self.timestampLabel, "messageLabel": self.messageLabel]
+        self.contentView.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|-(8)-[timestampLabel(80)]-(12)-[messageLabel]-(8)-|", options: NSLayoutFormatOptions.allZeros, metrics: nil, views: views))
+        self.contentView.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|-(4)-[messageLabel]-(4)-|", options: NSLayoutFormatOptions.allZeros, metrics: nil, views: views))
+        self.contentView.addConstraint(NSLayoutConstraint(item: self.timestampLabel, attribute: .Top, relatedBy: .Equal, toItem: self.messageLabel, attribute: .Top, multiplier: 1.0, constant: 0))
+    }
+    
+    class func attributedStringForTimestampLabel(line: ASLLine) -> NSAttributedString {
+        let string = NSDateFormatter.localizedStringFromDate(line.timestamp, dateStyle: .NoStyle, timeStyle: .LongStyle)
+        let attributes: [NSObject:AnyObject] = [
+            NSFontAttributeName: UIFont(name: "Courier", size: 12.0)!,
+            NSForegroundColorAttributeName: UIColor(white: 0.25, alpha: 1.0)
+        ]
+        return NSAttributedString(string: string, attributes: attributes)
+    }
+//
+//    class func attributedStringForMessageLabel(line: ASLLine) -> NSAttributedString {
+//        
+//    }
+    
+    class func heightWithLine(line: ASLLine) -> CGFloat {
+        return 128.0
     }
 }
 
@@ -49,7 +102,8 @@ class __ASLTableViewController: UITableViewController {
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as __ASLTableViewCell
-        cell.textLabel!.text = self.lines[indexPath.row].message
+        //cell.textLabel!.text = self.lines[indexPath.row].message
+        cell.line = self.lines[indexPath.row]
         return cell
     }
     
@@ -57,11 +111,19 @@ class __ASLTableViewController: UITableViewController {
         return nil
     }
     
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return __ASLTableViewCell.heightWithLine(self.lines[indexPath.row])
+    }
+    
     func onDoneButton(sender: AnyObject) {
         self.navigationController?.presentingViewController?.dismissViewControllerAnimated(true, completion: nil)
     }
 }
 
+/// A convenient view controller to display ASL lines in an app.
+/// 
+/// Just simply instantiate ASLViewController then present it by presentViewController:animated:completion:
+///
 public class ASLViewController: UINavigationController {
     
     public override init() {
